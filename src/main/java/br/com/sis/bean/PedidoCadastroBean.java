5c2a89ea97119dffb677b516d1e75cdb2781f055
William Gomes <william.desenv@gmail.com>
2020-10-22 00:17:54 -0400

    Upload e Download de pedidos de clientes

--- src/main/java/br/com/sis/bean/PedidoCadastroBean.java
+++ src/main/java/br/com/sis/bean/PedidoCadastroBean.java
@@ -1,18 +1,27 @@
 package br.com.sis.bean;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.hibernate.validator.constraints.NotEmpty;
 import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 import br.com.sis.entity.ItemOrcamento;
 import br.com.sis.entity.Orcamento;
+import br.com.sis.repository.OrcamentoRepository;
 import br.com.sis.service.OrcamentoService;
 import br.com.sis.util.jsf.FacesUtil;
 import lombok.Getter;
@@ -26,6 +35,9 @@ public class PedidoCadastroBean implements Serializable {
 
 	@Inject
 	private OrcamentoService orcamentoService;
+
+	@Inject
+	private OrcamentoRepository orcamentoRepository;
 
 	@Getter
 	@Setter
@@ -46,6 +58,10 @@ public class PedidoCadastroBean implements Serializable {
 
 	@Getter
 	private List<ItemOrcamento> itens;
+
+	@Getter
+	@Setter
+	private StreamedContent file;
 
 	public void inicializar() {
 		if (this.orcamento != null) {
@@ -74,23 +90,46 @@ public class PedidoCadastroBean implements Serializable {
 		this.orcamento = orcamentoService.salvar(this.orcamento);
 		FacesUtil.addInfoMessage("Pedido salvo com sucesso!");
 	}
-	
+
 	public boolean isTemArquivo() {
 		return this.orcamento.getNomeArquivo() != null;
 	}
-	
+
 	public void carregarArquivo(FileUploadEvent event) {
-		System.out.println("Executou");
 		if (event.getFile() != null) {
 			this.orcamento.setArquivo(event.getFile().getContent());
 			this.orcamento.setNomeArquivo(event.getFile().getFileName());
 			this.orcamento = orcamentoService.salvar(this.orcamento);
 			FacesUtil.addInfoMessage("Arquivo anexado com sucesso!");
-		}
-		else {
+		} else {
 			FacesUtil.addErroMessage("Sem arquivo para salvar");
 		}
-		
+
+	}
+
+	public void baixarPedido() {
+		gerarImagemDisco();
+		String nomeDownload = this.orcamento.getNomeArquivo();
+		file = DefaultStreamedContent.builder()
+                .contentType("application/pdf")
+                .name(nomeDownload)
+                .stream(() -> FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/resources/files/pedido.pdf"))
+                .build();
+	}
+
+	private void gerarImagemDisco() {
+		this.orcamento = orcamentoRepository.findById(this.orcamento.getId());
+		String local = FacesUtil.localArquivos();
+		String nome = "pedido.pdf";
+		Path localArquivo = FileSystems.getDefault().getPath(local, "");
+		local = local + File.separator + File.separator + nome;
+		System.out.println(local);
+		try {
+			Path fotoTemp = localArquivo.resolve(nome);
+			Files.write(fotoTemp, orcamento.getArquivo());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
